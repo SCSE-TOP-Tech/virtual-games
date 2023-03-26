e2cce98290943048cd4c6c8411c1ce50752a0eaf
eclarees <clareyangsuqi@gmail.com>
2023-03-27 01:39:45 +0800

    Initial Commit

---
+++ src/app/captainroom/page.jsx
@@ -0,0 +1,14 @@
+'use client'
+import { Container, Box, Text, StylesProvider } from '@chakra-ui/react'
+
+import Image from 'next/image'
+import styles from './components/styles.module.css'
+
+import background from '../../../public/Rooms/CaptainRoom/background.png'
+import musicalbums from '../../../public/Rooms/CaptainRoom/musicalbums.png'
+import spacegun from '../../../public/Rooms/CaptainRoom/spacegun.png'
+import lipstick from '../../../public/Rooms/CaptainRoom/lipstick.png'
+import brokenwatch from '../../../public/Rooms/CaptainRoom/brokenwatch.png'
+import note from '../../../public/Rooms/CaptainRoom/note.png'
+import guestbook from '../../../public/Rooms/CaptainRoom/guestbook.png'
+
