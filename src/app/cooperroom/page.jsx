54f172d6d298cdc82ff3eb2fba41e75974b68623
KZY20112001 <43703608+KZY20112001@users.noreply.github.com>
2023-05-02 00:35:33 +0800

    Fixed Image Scaling for CooperRoom

--- src/app/cooperroom/page.jsx
+++ src/app/cooperroom/page.jsx
@@ -1,5 +1,7 @@
+//test for image scaling
+//use cooper room as an example
 'use client'
-
+import CustomImage from '../components/CustomImage'
 import background from '../../../public/Rooms/CooperRoom/background.png'
 import luggage from '../../../public/Rooms/CooperRoom/luggage.png'
 import coffeemachine from '../../../public/Rooms/CooperRoom/coffeemachine.png'
@@ -11,83 +13,73 @@ import styles from './components/styles.module.css'
 
 import { Container, Text, Box } from '@chakra-ui/react'
 
-export default function CooperRoom() {
+export default function Test() {
   return (
     <Box w={['100%', '30em']} h='100%' p={4}>
       {/* background image */}
       <Container display='flex' justifyContent='space-around'>
         {/* placeholders for components  */}
-        <Box
-          color='red'
-          fontWeight='bold'
-          fontSize='2vh'
-          background='white'
-          p='10%'
-        >
+        <Box color='red' fontWeight='bold' fontSize='12px' p='5%'>
           Map placeholder
         </Box>
-        <Box color='red' fontWeight='bold' fontSize='2vh'>
+        <Box color='red' fontWeight='bold' fontSize='12px' p='5%'>
           Time placeholder
         </Box>
       </Container>
       <Box display='flex' justifyContent='center' zIndex='0' width='100%'>
         <Image src={background} alt='background' />
-        <Box position='absolute' zIndex='1'>
+        <Box position='absolute' zIndex='1' height='100%'>
           {/* luggage  */}
-          <Image
+          <CustomImage
             src={luggage}
             alt='luggage'
             className={styles.item}
-            style={{
-              position: 'relative',
-              right: '50%',
-              top: '80%',
-              width: '10vh',
-            }}
+            position='relative'
+            top={{ base: '32%', sm: '40%' }}
+            width={{ base: '110px', sm: '135px', md: '150px' }}
           />
+
           {/* newspaper  */}
-          <Image
+          <CustomImage
             src={newspaper}
             alt='newspaper'
             className={styles.item}
-            style={{
-              position: 'relative',
-              right: '40px',
-              top: '70px',
-              width: '70px',
-              filter: 'brightness(0.75)',
-            }}
+            position='relative'
+            top={{ base: '28%', sm: '35%' }}
+            right='35%'
+            width={{ base: '60px', sm: '80px' }}
+            filter='auto'
+            brightness='75%'
           />
+
           {/* id  */}
-          <Image
+          <CustomImage
             src={id}
             alt='id'
             className={styles.item}
-            style={{
-              position: 'relative',
-              right: '130px',
-              top: '250px',
-              width: '100px',
-              filter: 'brightness(0.75)',
-            }}
+            position='relative'
+            top={{ base: '25%', sm: '30%' }}
+            right='90%'
+            width={{ base: '90px', sm: '100px' }}
+            filter='auto'
+            brightness='70%'
           />
 
           {/* coffeemachine  */}
-          <Image
+          <CustomImage
             src={coffeemachine}
             alt='coffeemachine'
             className={styles.item}
-            style={{
-              position: 'relative',
-              left: '190px',
-              top: '50px',
-              width: '70px',
-              filter: 'brightness(0.75)',
-            }}
+            position='relative'
+            top={{ base: '5%', sm: '5%' }}
+            left={{ base: '130%', sm: '120%' }}
+            width={{ base: '60px', sm: '75px' }}
+            filter='auto'
+            brightness='75%'
           />
         </Box>
       </Box>
-      <Box position='absolute' bottom='10%' mt='2%' w='28em' background='white'>
+      <Box position='absolute' mt='2%' w={['100%', '28em']} background='white'>
         Text Component Here
       </Box>
     </Box>
