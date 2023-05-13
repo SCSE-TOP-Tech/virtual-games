8d3a530fa1e00028d2093b2423d18409ca64d823
KZY20112001 <43703608+KZY20112001@users.noreply.github.com>
2023-05-13 16:40:23 +0800

    added custom mobile themes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,39 @@
-'use client'
-import { CacheProvider } from '@chakra-ui/next-js'
-import { ChakraProvider, extendTheme, Box } from '@chakra-ui/react'
+"use client";
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider, extendTheme, Box } from "@chakra-ui/react";
 
 const theme = extendTheme({
-
-})
+  colors: {},
+  fonts: {},
+  fontSizes: {},
+  breakpoints: {
+    iphone_se: "375px",
+    iphone_xr: "414px",
+    iphone_12pro: "390px",
+    pixel_5: "393px",
+    galaxy_s8plus: "360px",
+    galaxy_s20ultra: "412px",
+  },
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body>
         <CacheProvider>
           <ChakraProvider theme={theme}>
-            <Box display='flex' bg='lightgreen' justifyContent='center' w='100%' h={800}>
+            <Box
+              display="flex"
+              bg="lightgreen"
+              justifyContent="center"
+              w="100%"
+              h={800}
+            >
               {children}
             </Box>
           </ChakraProvider>
         </CacheProvider>
       </body>
     </html>
-  )
+  );
 }
