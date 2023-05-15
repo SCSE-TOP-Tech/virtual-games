63baf238d3b57e772616bd385fdabf6a73942258
Eugene Wee <eugeneftw500@gmail.com>
2023-05-15 21:38:50 +0800

    Added Brand Room API

--- src/app/brandroom/page.jsx
+++ src/app/brandroom/page.jsx
@@ -1,58 +1,79 @@
-'use client'
-import background from '../../../public/Rooms/BrandRoom/brandbg.png'
-import phone from '../../../public/Rooms/BrandRoom/galaxy-phone.png'
+"use client";
+import background from "../../../public/Rooms/BrandRoom/brandbg.png";
+import phone from "../../../public/Rooms/BrandRoom/galaxy-phone.png";
 
-import Image from 'next/image'
-import styles from './components/styles.module.css'
-import { Container, Text, Box } from '@chakra-ui/react'
+import Image from "next/image";
+import styles from "./components/styles.module.css";
+import { Container, Text, Box } from "@chakra-ui/react";
+import CldImage from "../components/CldImage";
+import { useEffect, useState } from "react";
+import fetchRoom from "@/pages/api/rooms/fetchRoom";
 
 export default function BrandRoom() {
-  return (
-    <Box w={['100%', '30em']} h='100%' p={4}>
-      {/* background image */}
-      <Box
-        display='flex'
-        justifyContent='center'
-        zIndex='0'
-        h='90%'
-        width='100%'
-      >
-        <Container
-          position='absolute'
-          display='flex'
-          justifyContent='space-around'
-          mt='1%'
-        >
-          {/* placeholders for components  */}
-          <Text color='red' fontWeight='bold' fontSize='2vh'>
-            Map placeholder
-          </Text>
-          <Text color='red' fontWeight='bold' fontSize='2vh'>
-            Time placeholder
-          </Text>
-        </Container>
-        <Image src={background} alt='background' />
-        <Box position='absolute' zIndex='1'>
+  const [room, setRoom] = useState(false);
 
-          {/* galaxy phone */}
-          <Image
-            src={phone}
-            className={styles.item}
-            alt='album'
-            style={{
-              position: 'relative',
-              right: '29px',
-              top: '120px',
-              width: '100%',
-              margin: '0',
-            }}
-          />
-          
-        </Box>
-      </Box>
-      <Box position='absolute' bottom='10%' mt='2%' w='28em' background='white'>
-        Text Component Here
-      </Box>
-    </Box>
-  )
+  // Initial Load
+  useEffect(() => {
+    fetchRoom("brand", true).then((data) => {
+      setRoom(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      {room && (
+        <div>
+          <Box w={["100%", "30em"]} h="100%" p={4}>
+            {/* background image */}
+            <Box
+              display="flex"
+              justifyContent="center"
+              zIndex="0"
+              h="90%"
+              width="100%"
+            >
+              <Container
+                position="absolute"
+                display="flex"
+                justifyContent="space-around"
+                mt="1%"
+              >
+                {/* placeholders for components  */}
+                <Text color="red" fontWeight="bold" fontSize="2vh">
+                  Map placeholder
+                </Text>
+                <Text color="red" fontWeight="bold" fontSize="2vh">
+                  Time placeholder
+                </Text>
+              </Container>
+              <CldImage item={room.background} />
+              <Box position="absolute" zIndex="1">
+                {/* galaxy phone */}
+                <CldImage
+                  item={room.clues.galaxy_phone}
+                  className={styles.item}
+                  style={{
+                    position: "relative",
+                    right: "29px",
+                    top: "120px",
+                    width: "100%",
+                    margin: "0",
+                  }}
+                />
+              </Box>
+            </Box>
+            <Box
+              position="absolute"
+              bottom="10%"
+              mt="2%"
+              w="28em"
+              background="white"
+            >
+              Text Component Here
+            </Box>
+          </Box>
+        </div>
+      )}
+    </div>
+  );
 }
