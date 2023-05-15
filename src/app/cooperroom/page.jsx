a7edf9c16e5bc322feec75abc8a9699ecb588c23
Eugene Wee <eugeneftw500@gmail.com>
2023-05-15 21:20:46 +0800

    merge with main=

--- src/app/cooperroom/page.jsx
+++ src/app/cooperroom/page.jsx
@@ -1,118 +1,106 @@
-'use client'
-import styles from './components/styles.module.css'
-import { Container, Text, Box } from '@chakra-ui/react'
-import { Suspense, useEffect, useState } from 'react'
-import fetchRoom from '@/pages/api/rooms/fetchRoom'
-import CldImage from '../components/CldImage'
+"use client";
+import styles from "./components/styles.module.css";
+import { Container, Text, Box } from "@chakra-ui/react";
+import { Suspense, useEffect, useState } from "react";
+import fetchRoom from "@/pages/api/rooms/fetchRoom";
+import CldImage from "../components/CldImage";
 
 export default function CooperPage() {
   const [room, setRoom] = useState(false);
-  
+
   // Initial Load
   useEffect(() => {
-    fetchRoom("cooper", true)
-      .then(data => {
-        setRoom(data);
-      })
+    fetchRoom("cooper", true).then((data) => {
+      setRoom(data);
+    });
   }, []);
 
   return (
-    <div> 
-      {room && 
-        (<div>
-    <Box w={['100%', '30em']} h='100%' p={4}>
-      {/* background image */}
-      <Container display='flex' justifyContent='space-around'>
-        {/* placeholders for components  */}
-        <Box
-          color='red'
-          fontWeight='bold'
-          fontSize='2vh'
-          background='white'
-          p='10%'
-        >
-          Map placeholder
-        </Box>
-        <Box color='red' fontWeight='bold' fontSize='2vh'>
-          Time placeholder
-        </Box>
-      </Container>
-      <Box display='flex' justifyContent='center' zIndex='0' width='100%'>
-        <CldImage
-          item={room.background}
-        />
-        <Box position='absolute' zIndex='1'>
-          {/* luggage  */}
-          <CldImage
-            item={room.dummy_objects.luggage}
-            className={styles.item}
-            style={{
-              position: 'relative',
-              right: '50px',
-              top: '320px',
-              width: '150px',
-            }}
-          />
-          {/* newspaper  */}
-          <Image
-            src={newspaper}
-            alt='newspaper'
-            className={styles.item}
-            style={{
-              position: 'relative',
-              right: '40px',
-              top: '70px',
-              width: '70px',
-              filter: 'brightness(0.75)',
-            }}
-          />
-          {/* id  */}
-          <CldImage
-            item={room.dummy_objects.spaceID_card}
+    <div>
+      {room && (
+        <div>
+          <Box w={["100%", "30em"]} h="100%" p={4}>
+            {/* background ima`ge */}
+            <Container display="flex" justifyContent="space-around">
+              {/* placeholders for components  */}
+              <Box
+                color="red"
+                fontWeight="bold"
+                fontSize="2vh"
+                background="white"
+                p="10%"
+              >
+                Map placeholder
+              </Box>
+              <Box color="red" fontWeight="bold" fontSize="2vh">
+                Time placeholder
+              </Box>
+            </Container>
+            <Box display="flex" justifyContent="center" zIndex="0" width="100%">
+              <CldImage item={room.background} />
+              <Box position="absolute" zIndex="1">
+                {/* luggage  */}
+                <CldImage
+                  item={room.dummy_objects.luggage}
+                  className={styles.item}
+                  style={{
+                    position: "relative",
+                    right: "50px",
+                    top: "320px",
+                    width: "150px",
+                  }}
+                />
+                {/* newspaper  */}
+                <CldImage
+                  item={room.dummy_objects.newspaper}
+                  className={styles.item}
+                  style={{
+                    position: "relative",
+                    right: "40px",
+                    top: "70px",
+                    width: "70px",
+                    filter: "brightness(0.75)",
+                  }}
+                />
+                {/* id  */}
+                <CldImage
+                  item={room.dummy_objects.spaceID_card}
+                  className={styles.item}
+                  style={{
+                    position: "relative",
+                    right: "130px",
+                    top: "250px",
+                    width: "100px",
+                    filter: "brightness(0.75)",
+                  }}
+                />
 
-            className={styles.item}
-            style={{
-              position: 'relative',
-              right: '130px',
-              top: '250px',
-              width: '100px',
-              filter: 'brightness(0.75)',
-            }}
-          />
-
-          {/* coffeemachine  */}
-          <CldImage
-                item={room.dummy_objects.coffee_machine}
-
-            className={styles.item}
-            style={{
-              position: 'relative',
-              left: '200px',
-              top: '150px',
-              width: '100px',
-              filter: 'brightness(0.75)',
-            }}
-          />
-
-          {/* newspaper  */}
-          <CldImage
-                item={room.dummy_objects.newspaper}
-            className={styles.item}
-            style={{
-              position: 'relative',
-              right: '40px',
-              top: '11px',
-              width: '90px',
-              filter: 'brightness(0.75)',
-            }}
-          />
-        </Box>
-      </Box>
-      <Box position='absolute' bottom='10%' mt='2%' w='28em' background='white'>
-        Text Component Here
-      </Box>
-    </Box>
-    </div>)}
+                {/* coffeemachine  */}
+                <CldImage
+                  item={room.dummy_objects.coffee_machine}
+                  className={styles.item}
+                  style={{
+                    position: "relative",
+                    left: "200px",
+                    top: "150px",
+                    width: "100px",
+                    filter: "brightness(0.75)",
+                  }}
+                />
+              </Box>
+            </Box>
+            <Box
+              position="absolute"
+              bottom="10%"
+              mt="2%"
+              w="28em"
+              background="white"
+            >
+              Text Component Here
+            </Box>
+          </Box>
+        </div>
+      )}
     </div>
-  )
+  );
 }
--- src/app/seraphineroom/page.jsx
+++ src/app/seraphineroom/page.jsx
@@ -1,102 +1,110 @@
-'use client'
-import fetchRoom from '@/pages/api/rooms/fetchRoom';
-import { Container, Box, Text } from '@chakra-ui/react'
+"use client";
+import fetchRoom from "@/pages/api/rooms/fetchRoom";
+import { Container, Box, Text } from "@chakra-ui/react";
 
-import { useEffect, useState } from 'react';
-import CldImage from '../components/CldImage';
-import styles from './components/styles.module.css'
+import { useEffect, useState } from "react";
+import CldImage from "../components/CldImage";
+import styles from "./components/styles.module.css";
 
 export default function SeraphineRoom() {
   const [room, setRoom] = useState(false);
-  
+
   // Initial Load
   useEffect(() => {
-    fetchRoom("seraphine", true)
-      .then(data => {
-        setRoom(data);
-      })
+    fetchRoom("seraphine", true).then((data) => {
+      setRoom(data);
+    });
   }, []);
 
   return (
-    //bound to mobile view
-    <Box w={['100%', '30em']} h='100%' p={4}>
-      {/* container for background image and items*/}
+    <div>
       {room && (
-      {/* map and time components */}
-      <Container display='flex' justifyContent='space-around'>
-        {/* placeholders for components  */}
-        <Box color='red' fontWeight='bold' fontSize='2vh' p='10%'>
-          Map placeholder
-        </Box>
-        <Box color='red' fontWeight='bold' fontSize='2vh' p='10%'>
-          Time placeholder
-        </Box>
-      </Container>
-      <Box display='flex' justifyContent='center' zIndex='0' width='100%'>
-        {/* background image */}
-        <CldImage 
-            item={room.background}
-             />
-            {/* items container */}
-        <Box position='absolute' zIndex='1'>
-          {/*all dimensions are calculated manually lol */}
-          <CldImage
-              item={room.dummy_objects.teddybear}
-              className={styles.item}
-            alt='teddybear'
-            style={{
-              position: 'relative',
-              top: '325px',
-              left: '40px',
-              width: '55px',
-            }}
-          />
+        <div>
+          {/* bound to mobile view */}
+          <Box w={["100%", "30em"]} h="100%" p={4}>
+            {/* container for background image and items*/}
+            {/* map and time components */}
+            <Container display="flex" justifyContent="space-around">
+              {/* placeholders for components  */}
+              <Box color="red" fontWeight="bold" fontSize="2vh" p="10%">
+                Map placeholder
+              </Box>
+              <Box color="red" fontWeight="bold" fontSize="2vh" p="10%">
+                Time placeholder
+              </Box>
+            </Container>
+            <Box display="flex" justifyContent="center" zIndex="0" width="100%">
+              {/* background image */}
+              <CldImage item={room.background} />
+              {/* items container */}
+              <Box position="absolute" zIndex="1">
+                {/*all dimensions are calculated manually lol */}
+                <CldImage
+                  item={room.dummy_objects.teddybear}
+                  className={styles.item}
+                  alt="teddybear"
+                  style={{
+                    position: "relative",
+                    top: "325px",
+                    left: "40px",
+                    width: "55px",
+                  }}
+                />
 
-          {/*Jewelry box */}
-          <CldImage
-              item={room.dummy_objects.jewelrybox}
-              className={styles.item}
-            alt='jewelry box'
-            style={{
-              position: 'relative',
-              top: '255px',
-              right: '130px',
+                {/*Jewelry box */}
+                <CldImage
+                  item={room.dummy_objects.jewelrybox}
+                  className={styles.item}
+                  alt="jewelry box"
+                  style={{
+                    position: "relative",
+                    top: "255px",
+                    right: "130px",
 
-              width: '80px',
-            }}
-          />
+                    width: "80px",
+                  }}
+                />
 
-          {/* Lipstick */}
-          <CldImage
-              item={room.clues.lipstick}
-            className={styles.item}
-            alt='lipstick'
-            style={{
-              position: 'relative',
-              top: '450px',
-              right: '120px',
-              width: '40px',
-              filter: 'brightness(0.75)',
-            }}
-          />
+                {/* Lipstick */}
+                <CldImage
+                  item={room.clues.lipstick}
+                  className={styles.item}
+                  alt="lipstick"
+                  style={{
+                    position: "relative",
+                    top: "450px",
+                    right: "120px",
+                    width: "40px",
+                    filter: "brightness(0.75)",
+                  }}
+                />
 
-          {/* camera */}
-          <CldImage
-              item={room.dummy_objects.camera}
-            className={styles.item}
-            alt='camera'
-            style={{
-              position: 'relative',
-              left: '140px',
-              top: '215px',
-              width: '60px',
-            }}
-          />
-        </Box>
-      </Box>
-      <Box position='absolute' bottom='10%' mt='2%' w='28em' background='white'>
-        Text Component Here
-      </Box>
-    </Box>
-  )
+                {/* camera */}
+                <CldImage
+                  item={room.dummy_objects.camera}
+                  className={styles.item}
+                  alt="camera"
+                  style={{
+                    position: "relative",
+                    left: "140px",
+                    top: "215px",
+                    width: "60px",
+                  }}
+                />
+              </Box>
+            </Box>
+            <Box
+              position="absolute"
+              bottom="10%"
+              mt="2%"
+              w="28em"
+              background="white"
+            >
+              Text Component Here
+            </Box>
+          </Box>
+        </div>
+      )}
+    </div>
+  );
 }
