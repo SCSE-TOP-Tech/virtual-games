ac0eb797405573501d81a2a7d86b858117b5ff95
KZY20112001 <43703608+KZY20112001@users.noreply.github.com>
2023-05-28 13:06:32 +0800

    Update page.jsx

--- src/app/cooperroom/page.jsx
+++ src/app/cooperroom/page.jsx
@@ -5,19 +5,11 @@ import { Suspense, useEffect, useState } from "react";
 import fetchRoom from "@/pages/api/rooms/fetchRoom";
 import { CldImage, ItemImage, SizeFormatter } from "../components/ImageComp";
 import Map from "../Map";
+import Hint from "../components/Hint";
 export default function CooperPage() {
+  const [isOpenHint, setIsOpenHint] = useState(true);
   const [room, setRoom] = useState(false);
-  const a = SizeFormatter();
-  const b = {
-    iphone_se: "0",
-    iphone_xr: "8rem",
-    iphone_12pro: "0",
-    pixel_5: "0",
-    galaxy_s8plus: "0",
-    galaxy_s20ultra: "0",
-    ipad_air: "0",
-    ipad_mini: "0",
-  };
+
   //console.log(a.iphone_se === b.iphone_se);
   //console.log(isEqual(a, b));
   // Initial Load
@@ -168,6 +160,7 @@ export default function CooperPage() {
                 />
               </Box>
             </Box>
+            <Hint hints="test" isOpen={isOpenHint} setIsOpen={setIsOpenHint} />
             <Box
               position="absolute"
               bottom="10%"
