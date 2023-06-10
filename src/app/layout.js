ca28498a6d2f6699d3c713001ac8a7ecaa9e68e5
KZY20112001 <43703608+KZY20112001@users.noreply.github.com>
2023-06-10 23:56:35 +0800

    [VG-8] Fix Image Scaling for Cooper Room and Romily Room (#21)
    * minor change
    
    * Fixed Image Scaling for CooperRoom
    
    * minor comment added
    
    * Update page.jsx
    
    * changed image
    
    * added custom mobile themes
    
    * added mobile themes to cooper page
    
    * initial commit
    
    * removed renders
    
    * fix: made parent component have relative and child have absolute
    
    * Fetch Description for Carmen Room
    
    * minor change
    
    * Fixed Image Scaling for CooperRoom
    
    * minor comment added
    
    * added custom mobile themes
    
    * change clickHandler to onClick
    
    * fix: sizing and positioning
    
    * minor change
    
    * Fixed Image Scaling for CooperRoom
    
    * minor comment added
    
    * added custom mobile themes
    
    * Fixed Image Scaling for CooperRoom
    
    * feat: wrapper for image
    
    * added default values for Sizeformatter
    
    * added some sizing for luggage
    
    * feat: added responsive images for cooperroom
    
    * added brightness
    
    * refactor: removed unnecessary code
    
    * refactor: cleaned up css for cooper room
    
    * refactor: adjusted image sizes for romily room
    
    * feat: removed mobile test
    
    ---------
    
    Co-authored-by: eclarees <clareyangsuqi@gmail.com>
    Co-authored-by: JeremyNathanJusuf <Jeremynathan0502@gmail.com>

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,36 @@
-'use client'
-import { CacheProvider } from '@chakra-ui/next-js'
-import { ChakraProvider, extendTheme, Box } from '@chakra-ui/react'
+"use client";
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider, extendTheme, Box } from "@chakra-ui/react";
 
 const theme = extendTheme({
-
-})
+  colors: {},
+  fonts: {},
+  fontSizes: {},
+  breakpoints: {
+    iphone_se: "375px",
+    iphone_xr: "414px",
+    iphone_12pro: "390px",
+    pixel_5: "393px",
+    galaxy_s8plus: "360px",
+    galaxy_s20ultra: "412px",
+    ipad_air: "820px",
+    ipad_mini: "768px",
+  },
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body>
         <CacheProvider>
           <ChakraProvider theme={theme}>
-            <Box display='flex' bg='lightgreen' justifyContent='center' w='100%' h={800}>
+            <Box
+              display="flex"
+              bg="lightgreen"
+              justifyContent="center"
+              w="100%"
+              h={800}
+            >
               {children}
             </Box>
           </ChakraProvider>
@@ -20,5 +38,5 @@ export default function RootLayout({ children }) {
       </body>
 
     </html>
-  )
+  );
 }
