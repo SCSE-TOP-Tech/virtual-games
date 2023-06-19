b381374520527bb653b508210782acf83cbdf555
JeremyNathanJusuf <Jeremynathan0502@gmail.com>
2023-06-19 16:40:40 +0700

    fix: maan room

--- src/app/maanroom/page.jsx
+++ src/app/maanroom/page.jsx
@@ -5,6 +5,7 @@ import { Container, Text, Box } from "@chakra-ui/react";
 import { CldImage, ItemImage, SizeFormatter } from "../components/ImageComp";
 import { Suspense, useEffect, useState } from "react";
 import fetchRoom from "@/pages/api/rooms/fetchRoom";
+import Hint from "../components/Hint";
 
 export default function MaanRoom() {
   const [room, setRoom] = useState(false);
@@ -19,54 +20,63 @@ export default function MaanRoom() {
   return (
     <Suspense fallback={<h1>Loading</h1>}>
       {room && (
-        <div>
-          <Box w={["100%", "30em"]} h="100%" p={4}>
-            {/* background image */}
-            <Box
-              display="flex"
-              justifyContent="center"
-              zIndex="0"
-              h="90%"
-              width="100%"
-            >
-              <Container
-                position="absolute"
-                display="flex"
-                justifyContent="space-around"
-                mt="1%"
-              >
-                {/* placeholders for components  */}
-                <Map />
-                <Text color="red" fontWeight="bold" fontSize="2vh">
-                  Time placeholder
-                </Text>
-              </Container>
-              <CldImage item={room.background} />
-              <Box position="absolute" zIndex="1">
-                {/* spacesword  */}
-                <CldImage
+        <Box w={["100%", "30em"]} h="100%" p={4} position="relative">
+          <Container display="flex" justifyContent="space-around">
+            <Map />
+            <Box color="red" fontWeight="bold" fontSize="2vh">
+              Time placeholder
+            </Box>
+          </Container>
+          <Box
+            display="flex"
+            justifyContent="center"
+            position="relative"
+            width="100%"
+          >
+            <CldImage item={room.background} />
+            <Box position="absolute" zIndex="1">
+
+              {/* spacesword  */}
+              <Hint>
+                <ItemImage
                   item={room.dummy_objects.spacesword}
                   className={styles.item}
-                  style={{
-                    position: "relative",
-                    right: "5px",
-                    top: "370px",
-                    width: "80px",
-                  }}
+                  width="2.7rem"
+                  filter="auto"
+                  brightness="75%"
+                  right={SizeFormatter(
+                    "0rem", //iphone se
+                    "0rem", //iphone xr
+                    "0rem", //iphone 12pro
+                    "0em", //pixel 5
+                    "0rem", //samsung galaxy s8+
+                    "0rem", //samsung galaxy s20 ultra
+                    "0em", //ipad air
+                    "0rem" //ipad mini
+                  )}
+                  top={SizeFormatter(
+                    "11rem",
+                    "12.5rem",
+                    "11.7rem",
+                    "11.7rem",
+                    "10.4rem",
+                    "12.4rem",
+                    "15rem",
+                    "15.2rem"
+                  )}
                 />
-              </Box>
-            </Box>
-            <Box
-              position="absolute"
-              bottom="10%"
-              mt="2%"
-              w="28em"
-              background="white"
-            >
-              Text Component Here
+              </Hint>
+
             </Box>
           </Box>
-        </div>
+          <Box
+            mt="2%"
+            w="100%"
+            background="white"
+          >
+            Text Component Here
+          </Box>
+        </Box>
       )}
     </Suspense>
   );
