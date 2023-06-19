af093469457036a90daf39c596171472ea4097a6
JeremyNathanJusuf <Jeremynathan0502@gmail.com>
2023-06-19 18:02:43 +0700

    fix: Carmen room aspect ratio

--- src/app/carmenroom/page.jsx
+++ src/app/carmenroom/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import styles from './components/styles.module.css'
 import { Container, Text, Box } from '@chakra-ui/react'
-import { ItemImage, SizeFormatter, CustomBgImage } from "../components/ImageComp";
+import { ItemImage, SizeFormatter, CustomBgImage, CldImage } from "../components/ImageComp";
 import { Suspense, useEffect, useState } from 'react'
 import fetchRoom from '@/pages/api/rooms/fetchRoom'
 import Map from "../Map";
@@ -9,7 +9,7 @@ import Hint from '../components/Hint';
 
 export default function CarmenRoom() {
   const [room, setRoom] = useState(false);
-  
+
   // Initial Load
   useEffect(() => {
     fetchRoom("carmen", true)
@@ -18,97 +18,101 @@ export default function CarmenRoom() {
       })
   }, []);
 
-  const textHandler = (text) => { 
+  const textHandler = (text) => {
     setText(text);
   }
   return (
-    <Suspense fallback={<h1>Loading</h1>}> 
-    {room && 
-      (<Box w={['100%', '30rem']} h='100%' p={4} position='relative'>
-        <Container display="flex" justifyContent="space-around">
+    <Suspense fallback={<h1>Loading</h1>}>
+      {room &&
+        (<Box w={["100%", "30em"]} h="100%" p={4} position="relative">
+          <Container display="flex" justifyContent="space-around">
             <Map />
             <Box color="red" fontWeight="bold" fontSize="2vh">
               Time placeholder
             </Box>
           </Container>
           <Box
-            display='flex'
-            justifyContent='center'
-            zIndex='0'
-            h='90%'
-            width='100%'
+            display="flex"
+            justifyContent="center"
+            position="relative"
+            width="100%"
           >
             {/* background image */}
-            <CustomBgImage item={room.background} height='58%'/>
+            <CldImage item={room.background} />
             {/* items */}
             <Box position='absolute' zIndex='1'>
               {/* mail */}
               <Hint>
-              <ItemImage
-                item={room.clues.mail}
-                className={styles.item}
-                width='5.9rem'
-                right={SizeFormatter(
-                  "8rem", //iphone se
-                  "9rem", //iphone xr
-                  "8.3rem", //iphone 12pro
-                  "8.3rem", //pixel 5
-                  "7.8rem", //samsung galaxy s8+
-                  "8.7rem", //samsung galaxy s20 ultra
-                  "10.2rem", //ipad air
-                  "10.2rem" //ipad mini
-                )}
-                top={SizeFormatter(
-                  "11rem",
-                  "10.9rem",
-                  "10.9rem",
-                  "10.9rem",
-                  "10.9rem",
-                  "10.9rem",
-                  "10.9rem",
-                  "10.9rem"
-                )}
-              />
+                <ItemImage
+                  item={room.clues.mail}
+                  className={styles.item}
+                  width='5.9rem'
+                  filter="auto"
+                  brightness='76%'
+                  right={SizeFormatter(
+                    "8rem", //iphone se
+                    "9rem", //iphone xr
+                    "8.3rem", //iphone 12pro
+                    "8.3rem", //pixel 5
+                    "7.8rem", //samsung galaxy s8+
+                    "8.7rem", //samsung galaxy s20 ultra
+                    "10.2rem", //ipad air
+                    "10.2rem" //ipad mini
+                  )}
+                  top={SizeFormatter(
+                    "8.8rem",
+                    "10.3rem",
+                    "9.3rem",
+                    "9.6rem",
+                    "8.3rem",
+                    "10.2rem",
+                    "12.7rem",
+                    "12.7rem"
+                  )}
+                />
               </Hint>
-              
+
               {/* master key */}
               <Hint>
-              <ItemImage
-                item={room.clues.master_key}
-                className={styles.item}
-                width='2rem'
-                right={SizeFormatter(
-                  "0rem", //iphone se
-                  "1rem", //iphone xr
-                  "1rem", //iphone 12pro
-                  "1rem", //pixel 5
-                  "1rem", //samsung galaxy s8+
-                  "1.3rem", //samsung galaxy s20 ultra
-                  "1.3rem", //ipad air
-                  "1.4rem" //ipad mini
-                )}
-                top={SizeFormatter(
-                  "16.7rem",
-                  "16.7rem",
-                  "16.3rem",
-                  "16.4rem",
-                  "16.3rem",
-                  "16.5rem",
-                  "16.5rem",
-                  "16.5rem"
-                )}
-              />
+                <ItemImage
+                  item={room.clues.master_key}
+                  className={styles.item}
+                  width='2rem'
+                  filter="auto"
+                  brightness='76%'
+                  right={SizeFormatter(
+                    "0rem", //iphone se
+                    "1rem", //iphone xr
+                    "1rem", //iphone 12pro
+                    "1rem", //pixel 5
+                    "1rem", //samsung galaxy s8+
+                    "1.3rem", //samsung galaxy s20 ultra
+                    "1.3rem", //ipad air
+                    "1.4rem" //ipad mini
+                  )}
+                  top={SizeFormatter(
+                    "13rem",
+                    "15.7rem",
+                    "14rem",
+                    "14.4rem",
+                    "12.3rem",
+                    "15.5rem",
+                    "19.5rem",
+                    "19.5rem"
+                  )}
+                />
               </Hint>
-              
+
               {/* clothspin */}
               <Hint>
                 <ItemImage
                   item={room.dummy_objects.clothespin}
                   className={styles.item}
-                  location={['30rem', '-15rem', 'unset', 'unset']}
                   width='1.5rem'
+                  filter="auto"
+                  brightness='20%'
                   right={SizeFormatter(
-                    "-10rem", //iphone se
+                    "-11rem", //iphone se
                     "-12.2rem", //iphone xr
                     "-12rem", //iphone 12pro
                     "-12rem", //pixel 5
@@ -118,14 +122,14 @@ export default function CarmenRoom() {
                     "-14rem" //ipad mini
                   )}
                   top={SizeFormatter(
-                    "16rem",
-                    "16rem",
-                    "16rem",
-                    "16rem",
-                    "16rem",
-                    "16rem",
-                    "16rem",
-                    "16rem"
+                    "12.7rem",
+                    "15rem",
+                    "13.6rem",
+                    "14rem",
+                    "11.8rem",
+                    "15rem",
+                    "19rem",
+                    "19rem"
                   )}
                 />
               </Hint>
@@ -139,7 +143,7 @@ export default function CarmenRoom() {
             Text Component Here
           </Box>
         </Box>
-      )}
+        )}
     </Suspense>
   )
 }
