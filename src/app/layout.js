ee9a09e3594ae0622090088a9854e66dae056894
KZY20112001 <43703608+KZY20112001@users.noreply.github.com>
2023-07-09 01:56:29 +0800

    feat: added UI skeleton

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,7 +36,6 @@ export default function RootLayout({ children }) {
           </ChakraProvider>
         </CacheProvider>
       </body>
-
     </html>
   );
 }
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -4,15 +4,21 @@ import * as yup from "yup";
 import { useFormik } from "formik";
 import {
   Box,
+  Container,
+  Heading,
   Input,
   InputGroup,
   InputLeftElement,
   InputRightElement,
   Text,
 } from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function Login() {
+  const [error, setError] = useState(false);
+
   const handleSubmit = async (e) => {
+    console.log(formik.values.username);
     e.preventDefault();
   };
   const formik = useFormik({
@@ -34,6 +40,67 @@ export default function Login() {
     }),
   });
   return (
-    <Box w={["100%", "30em"]} h="100%" p={4} background="transparent"></Box>
+    <Container
+      w={["100%", "30em"]}
+      h="100%"
+      centerContent
+      justifyContent="center"
+      fontFamily="Open Sans"
+    >
+      <Box
+        background="yellow.300"
+        h="fit-content"
+        display="flex"
+        rounded="3xl"
+        flexDir="column"
+        p={10}
+        gap={4}
+      >
+        <Heading fontSize="lg" as="i" textColor="gray.600">
+          Welcome To Our Virtual Game
+        </Heading>
+        <InputGroup size="lg" backgroundColor="lightblue" borderRadius="2xl">
+          <Text fontWeight={550} pt={1} m={2}>
+            Username
+          </Text>
+          <Input
+            value={formik.values.username}
+            onChange={formik.handleChange}
+            required
+            id="username"
+            type="text"
+            fontSize="sm"
+          />
+        </InputGroup>
+        <InputGroup size="lg" backgroundColor="lightblue" borderRadius="2xl">
+          <Text fontWeight={550} pt={1} m={2}>
+            Password
+          </Text>
+          <Input
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            required
+            id="password"
+            type="text"
+            fontSize="sm"
+          />
+        </InputGroup>
+
+        <Box display="flex" justifyContent="center">
+          <Text
+            as="button"
+            px={4}
+            rounded="2xl"
+            fontWeight={600}
+            py={1}
+            background="whiteAlpha.700"
+            textColor="gray.600"
+            onClick={handleSubmit}
+          >
+            Enter!
+          </Text>
+        </Box>
+      </Box>
+    </Container>
   );
 }
