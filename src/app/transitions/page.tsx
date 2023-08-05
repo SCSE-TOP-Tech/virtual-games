17782cc3bbb6ae08a877e0574ea6a338c842e152
iujinwee <eugeneftw500@gmail.com>
2023-08-05 11:00:42 +0800

    Removed unncessary code

--- src/app/transitions/page.tsx
+++ src/app/transitions/page.tsx
@@ -22,7 +22,6 @@ interface image {
 export default function Transitions() {
   const router = useRouter();
   const [user, setUser] = useState<User>();
-  const [transition, setTransition] = useState<Transition>();
   const [image, setImage] = useState<image>();
   const [text, setText] = useState<string>();
 
@@ -33,7 +32,6 @@ export default function Transitions() {
         setUser(currentUser);
         const trans = await fetchTransition(currentUser.transitionID);
         if (trans) {
-          setTransition(trans);
           setText(trans.dialog);
           setImage(trans.image);
         }
@@ -70,49 +68,6 @@ export default function Transitions() {
         width={image?.width}
       />
     );
-  };
-
-  const storyline = (transitionStage) => {
-    switch (transitionStage) {
-      case 0:
-        return {
-          text:
-            "In a distant corner of the universe lies the Black Eye Galaxy, ruled\n" +
-            "          by the almighty King White. This kingdom holds a precious artifact\n" +
-            "          known as the space stone which has incredible value and is carefully\n" +
-            "          stored within the Tesseract. The responsibility of protecting this\n" +
-            "          invaluable stone falls upon the Knight of Interstellar, a loyal\n" +
-            "          guardian in service to King White.",
-          image: (
-            <Image
-              src="/intro/knight.jpg"
-              alt="knight"
-              width={200}
-              height={200}
-            />
-          ),
-        };
-      case 1:
-        return {
-          text:
-            "On a fine evening, Princess White, the daughter of King White, invited\n" +
-            "          seven legendary heroes to attend the annual interstellar meeting held\n" +
-            "          within a master spaceship.",
-          image: <Image src="/intro/princess.jpg" width={100} height={100} />,
-        };
-      default:
-        return {
-          text:
-            "Concerned for the safety of his daughter, King White entrusts the\n" +
-            "          Knight with a crucial task. The space stone must be kept secure, yet\n" +
-            "          remain in close proximity to the Princess. Thus, the Knight is\n" +
-            "          commanded to place the tesseract within a safe stored in Princess\n" +
-            "          White's room. In doing so, the Knight is able to protect both the\n" +
-            "          Princess and the Tesseract. However, little do they know, the stage is\n" +
-            "          set for an epic adventure to unfold.",
-          image: <Image src="/intro/stone.jpg" width={300} height={300} />,
-        };
-    }
   };
 
   return (
